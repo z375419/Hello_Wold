0ac827367f88ed938b57aea751dd92f88eefba40
z375419 <45584245+z375419@users.noreply.github.com>
2023-11-28 22:21:04 +0800

    Update and rename movefolder to movefolder.py

--- movefolder.py
+++ movefolder.py
@@ -17,3 +17,19 @@ for folder in folders:
     shutil.copytree(folder, new_folder_path)
     with open(os.path.join(new_folder_path, 'status.txt'), 'w', encoding='utf-8') as f:
         f.write('转换pdf完成')
+
+
+import os
+import json
+base_folder = r'D:\python\env\uiautobot\ExtractPDfInformation\data\未完成'
+for folder_name in os.listdir(base_folder):
+    folder_path = os.path.join(base_folder, folder_name)
+    for file_name in os.listdir(folder_path):
+        if '.json' in file_name and file_name not in ['status.json', 'word.json']:
+            try:
+                file_path = os.path.join(folder_path, file_name)
+                jsdata = json.load(open(file_path, 'r', encoding='utf-8'))
+                print(folder_name, jsdata['cust']['date_year'])
+            except Exception as e:
+                print(folder_name, str(e))
+
